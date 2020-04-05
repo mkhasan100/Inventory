d0300a097fa908f30e474d79fa1ad8b79a989a04
hasan3848 <hasan3848@gmail.com>
2020-04-05 11:58:08 +0600

    New table

---
+++ SellerPoint/SellerPoint/Models/DealerSaleProductDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellerPoint.Models
+{
+    [Serializable]
+    public class DealerSaleProductDetail
+    {
+        [Key]
+        public int Id { get; set; }
+        public int DealerSaleId { get; set; }
+        public int ProductId { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public int SaleTypeId { get; set; }
+
+
+    }
+}
