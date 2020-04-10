42384e33f14f56ee8f6d8bfaea137a4aa3001370
syedmdkamruzzaman <syedmdkamruzzaman@gmail.com>
2020-04-10 21:53:05 +0600

    excel and csv download added

--- SellerPoint/SellerPoint/Controllers/DealerSalesController.cs
+++ SellerPoint/SellerPoint/Controllers/DealerSalesController.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using SellerPoint.Data;
 using SellerPoint.Models;
 using SellerPoint.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 
 namespace SellerPoint.Controllers
 {
     public class DealerSalesController : Controller
     {
         private readonly SellerPointContext _context;
+        private IWebHostEnvironment _hostingEnvironment;
 
-        public DealerSalesController(SellerPointContext context)
+        //[Obsolete]
+        public DealerSalesController(SellerPointContext context, IWebHostEnvironment hostingEnvironment)
         {
             _context = context;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         // GET: DealerSales
@@ -263,6 +273,108 @@ namespace SellerPoint.Controllers
             return Json(Product);
         }
 
-        
+        //[HttpGet]
+        //[Route("Export")]
+        public IActionResult Export()
+        {
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string sFileName = @"DealerSales.csv";
+            string fileName = Path.Combine(sWebRootFolder, sFileName);
+            string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, sFileName);
+            FileInfo file = new FileInfo(fileName);
+            if (file.Exists)
+            {
+                file.Delete();
+                file = new FileInfo(fileName);
+            }
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                // add a new worksheet to the empty workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Sheet1");
+                //First add the headers
+                worksheet.Cells[1, 1].Value = "Dealer";
+                worksheet.Cells[1, 2].Value = "Unit Price";
+                worksheet.Cells[1, 3].Value = "Discount";
+                worksheet.Cells[1, 4].Value = "Payable Total";
+                worksheet.Cells[1, 5].Value = "Due";
+                worksheet.Cells[1, 6].Value = "Order No";                
+
+                var DealerSalesList = _context.DealerSale.AsEnumerable()
+                                              .Join(_context.Dealer, ds=>ds.DealerId,d=>d.Id,(ds,d)=>new { ds,d})
+                                       .Select(s=>new { 
+                                           s.d.Name
+                                           ,s.ds.UnitPrice
+                                           ,s.ds.Discount
+                                           ,s.ds.payableTotal
+                                           ,s.ds.Due
+                                           ,s.ds.OrderNo                                            
+                                       }).ToList();
+
+
+                //Add values
+                for (int i = 0; i < DealerSalesList.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = DealerSalesList[i].Name;
+                    worksheet.Cells[i + 2, 2].Value = DealerSalesList[i].UnitPrice;
+                    worksheet.Cells[i + 2, 3].Value = DealerSalesList[i].Discount;
+                    worksheet.Cells[i + 2, 4].Value = DealerSalesList[i].payableTotal;
+                    worksheet.Cells[i + 2, 5].Value = DealerSalesList[i].Due;
+                    worksheet.Cells[i + 2, 6].Value = DealerSalesList[i].OrderNo;
+                }
+
+
+                package.Save(); //Save the workbook.
+            }
+
+            //"https://www.talkingdotnet.com/import-export-excel-asp-net-core-2-razor-pages/"
+            var result = PhysicalFile(fileName, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+            Response.Headers["Content-Disposition"] = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = file.Name
+            }.ToString();
+
+            return result;
+        }
+
+
+        public FileContentResult csv()
+        {
+            var DealerSalesList = _context.DealerSale.AsEnumerable()
+                                              .Join(_context.Dealer, ds=>ds.DealerId, d=>d.Id, (ds, d)=>new { ds,d
+    })
+                                       .Select(s=>new { 
+                                           s.d.Name
+                                           ,s.ds.UnitPrice
+                                           ,s.ds.Discount
+                                           ,s.ds.payableTotal
+                                           ,s.ds.Due
+                                           ,s.ds.OrderNo
+}).ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Dealer,Unit Price,Discount,Payable Total,Due,Order No");
+            sb.Append("\r\n");
+            for (int i = 0; i < DealerSalesList.Count; i++)
+            {
+                sb.Append(DealerSalesList[i].Name);
+                sb.Append(","+DealerSalesList[i].UnitPrice.ToString());
+                sb.Append("," + DealerSalesList[i].Discount.ToString());
+                sb.Append("," + DealerSalesList[i].payableTotal.ToString());
+                sb.Append("," + DealerSalesList[i].Due.ToString());
+                sb.Append("," + DealerSalesList[i].OrderNo);
+
+                //Append new line character.
+                sb.Append("\r\n");
+
+            }
+
+
+            return File(Encoding.UTF8.GetBytes(sb.ToString()), "text/csv", "Grid.csv");
+
+        }
+
+
     }
 }
--- SellerPoint/SellerPoint/Models/DealerSale.cs
+++ SellerPoint/SellerPoint/Models/DealerSale.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace SellerPoint.Models
 {
+    [Serializable]
     public class DealerSale
     {
         [Key]
