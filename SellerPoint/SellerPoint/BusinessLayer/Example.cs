c43287ab2a3ab5547a7e9f4f7c7cb4d0971bc1f7
hasan3848 <hasan3848@gmail.com>
2020-08-22 19:39:04 +0600

    new line added

---
+++ SellerPoint/SellerPoint/BusinessLayer/Example.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellerPoint.BusinessLayer
+{
+    public class Example
+    {
+    }
+}
